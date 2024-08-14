51d7cad7e4e58710d4244accd3b7c83f8f893b49
Hitesh <hiteshsharma.2314@gmail.com>
2024-08-14 21:57:38 +0530

    draft: next-auth credentials and google provider

--- packages/common/lib/auth.ts
+++ packages/common/lib/auth.ts
@@ -9,65 +9,95 @@ import { JWT } from "next-auth/jwt";
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
-        phone: { label: "Phone number", type: "text", placeholder: "1231231231" },
-        password: { label: "Password", type: "password" }
+        email: {
+          label: "email",
+          type: "text",
+          placeholder: "johndoe@example.com",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "password",
+        },
+        type: {
+          label: "type",
+          type: "text",
+          placeholder: "User or Merchant",
+        },
       },
-      // TODO: User credentials type from next-aut
-      async authorize(credentials: any) {
-        // Do zod validation, OTP validation here
-        const hashedPassword = await bcrypt.hash(credentials.password, 10);
-        const existingUser = await prisma.user.findFirst({
-          where: {
-            number: credentials.phone
-          }
-        });
-
-        if (existingUser) {
-          const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-          if (passwordValidation) {
-            return {
-              id: existingUser.id.toString(),
-              name: existingUser.name,
-              email: existingUser.number
-            }
-          }
-          return null;
-        }
-
+      async authorize(
+        credentials: Record<"email" | "password" | "type", string> | undefined,
+      ) {
         try {
-          const user = await prisma.user.create({
-            data: {
-              number: credentials.phone,
-              password: hashedPassword
-            }
+          if (!credentials?.email || !credentials?.password || !credentials?.type) return null;
+
+          const parsedData = credentialsSchema.safeParse(credentials);
+          if (!parsedData.success) return null;
+
+          const { email, password, type } = parsedData.data;
+
+          const existingAccount = await prisma.account.findUnique({
+            where: { email },
           });
 
-          return {
-            id: user.id.toString(),
-            name: user.name,
-            email: user.number
+          if (existingAccount) {
+            if (existingAccount.authType != "Email") return null
           }
-        } catch (e) {
-          console.error(e);
-        }
 
-        return null
+          if (existingAccount) {
+            const validPassword = await bcrypt.compare(
+              password,
+              String(existingAccount.password),
+            );
+            if (validPassword) return existingAccount;
+
+            return null;
+          }
+
+          const saltRounds = parseInt(process.env.SALT_ROUNDS || '10', 10)
+          const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+          const user = await prisma.account.create({
+            data: {
+              email,
+              password: hashedPassword,
+              type,
+              authType: "Email"
+            },
+          });
+
+          return user;
+        } catch (error) {
+          console.error("Error creating user:", error);
+
+          return null;
+        }
       },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-    },)
+    })
   ],
-  secret: process.env.JWT_SECRET || "secret",
+  secret: process.env.JWT_SECRET || "",
   callbacks: {
-    // TODO: can u fix the type here? Using any is bad
+    async signIn(params: any) {
+      console.log(params)
+
+      return true
+    },
+    async jwt({ token, account }: { token: JWT, account: Account | null }) {
+      console.log(token, account)
+
+      return token
+    },
     async session({ token, session }: any) {
+      console.log(token, session)
       session.user.id = token.sub
 
       return session
     }
   }
-}
+};
